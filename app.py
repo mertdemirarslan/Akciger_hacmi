b254cee36d2232b8bde1e0fafaeb0d7bb779bac0
mertdemirarslan <57152914+mertdemirarslan@users.noreply.github.com>
2021-03-17 15:02:05 +0300

    son

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open("model.pkl","rb"))
+model2 = pickle.load(open("model2.pkl","rb"))
 
 @app.route('/')
 def home():
@@ -19,7 +20,21 @@ def predict():
     output='{0:.{1}f}'.format(prediction[0][0], 2)
 
 
-    return render_template('index.html',pred='Akciğer hacmi {}'.format(str(float(output))))
+    return render_template('index.html',pred='Sag Akciğer Hacmi {}'.format(str(float(output))))
+
+@app.route("/predict2",methods=["POST"])
+def predict2():
+    features = [int(x) for x in request.form.values()]
+
+    final_features = [np.array(features)]
+    prediction2 = model2.predict(final_features)
+    print(str(prediction2))
+    output2='{0:.{1}f}'.format(prediction2[0][0], 2)
+
+
+    return render_template('index.html',pred='Sol Akciğer Hacmi {}'.format(str(float(output2))))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- model.py
+++ model.py
@@ -27,8 +27,22 @@ model = lm.fit(X_train, y_train)
 pipe=make_pipeline(StandardScaler(),lm)
 pipe.fit(X_train, y_train)
 pickle.dump(pipe, open("model.pkl","wb"))
-
 model = pickle.load(open("model.pkl","rb"))
+
+df2=pd.read_spss("akcigerhacmi.sav")
+
+A=df2.drop(["sagAK","solAK"],axis=1)
+b = df2[["solAK"]]
+
+lm2  = LinearRegression()
+A_train, A_test, b_train, b_test = train_test_split(A, b, test_size = 0.30, random_state = 1)
+
+model2 = lm2.fit(A_train, b_train)
+pipe2=make_pipeline(StandardScaler(),lm2)
+pipe2.fit(A_train, b_train)
+pickle.dump(pipe2, open("model2.pkl","wb"))
+model2 = pickle.load(open("model2.pkl","rb"))
+
 
 
 
